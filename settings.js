ae916f8ee0de9e4300ae8886847d656d0f81d351
KAVEESHA SITHUM <107849120+Kaveeshasithum@users.noreply.github.com>
2023-03-15 12:30:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kurunegala" //ur location
 
 //bot bomdy 
-global.session = "UHlHZkFYTTE=" //session Id
+global.session = "cXNNbTFMWGE=" //session Id
 global.ownernomer = "94786802371" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94786802371'] //ur premium number
